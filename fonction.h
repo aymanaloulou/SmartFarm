4e7d73d6a4d94e3a9e3d275d854a8f9d1f3eb049
Omar <omartn7400@gmail.com>
2020-10-22 19:37:07 +0100

    Atelier 0

---
+++ fonction.h
@@ -0,0 +1,6 @@
+#ifndef FONCTION_H_
+#define FONCTION_H_
+int sommer (int a,int b);
+int soustraire (int a,int b);
+float diviser (int a,int b);
+#endif
--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include "fonction.h"
 
 void main() {
 	int a;int b;
 	printf ("Donner deux entier : ");
 	scanf ("%d %d",&a,&b);
-	printf ("%d + %d = %d\n",a,b,a+b);
+	printf ("%d + %d = %d\n",a,b,sommer (a,b));
+	printf ("%d - %d = %d\n",a,b,soustraire (a,b));
+	printf ("%d / %d = %f\n",a,b,diviser (a,b));
 }
