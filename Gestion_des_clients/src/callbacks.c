6f1a68879186f935111e8ba64a86a8c89c0907ca
zeidiaymen <aymen.zeidi@esprit.tn>
2020-11-08 19:37:52 +0100

    interface Gestion des clients

---
+++ Gestion_des_clients/src/callbacks.c
@@ -0,0 +1,19 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_button_gestion_clients_clicked      (GtkWidget      *objet_graphique1,
+                                        gpointer         user_data)
+{
+
+
+}
+
---
+++ Gestion_des_clients/src/callbacks.h
@@ -0,0 +1,6 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button_gestion_clients_clicked      (GtkWidget       *objet_graphique1,
+                                        gpointer         user_data);
