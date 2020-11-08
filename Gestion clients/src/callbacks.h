15c1568f3d5481119f12314a9a6d5aab2ba3f0bf
zeidiaymen <aymen.zeidi@esprit.tn>
2020-11-08 20:46:16 +0100

    gestion des ouvrieres

---
+++ Gestion clients/src/callbacks.h
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+
+
+void
+on_button_gestion_clients_clicked      (GtkWidget       *objet_graphique1,
+                                        gpointer         user_data);
+
+void
+on_button_ajouter_client_clicked       (GtkWidget       *objet_graphique2,
+                                        gpointer         user_data);
+
+void
+on_button_modifier_client_clicked      (GtkWidget       *objet_graphique3,
+                                        gpointer         user_data);
+
+void
+on_button_annuler_aj_clicked           (GtkWidget       *objet_graphique4,
+                                        gpointer         user_data);
