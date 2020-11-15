f603aa315704327d216b09e65185c35696c65410
zeidiaymen <aymen.zeidi@esprit.tn>
2020-11-16 00:21:34 +0100

    atelier

---
+++ project3/src/callbacks.h
@@ -0,0 +1,19 @@
+#include <gtk/gtk.h>
+#include "string.h"
+#include<stdio.h>
+
+void
+on_cancelbutton1_clicked               (GtkWidget       *button1,
+                                        gpointer         user_data);
+
+void
+on_okbutton1_clicked                   (GtkWidget       *button1,
+                                        gpointer         user_data);
+
+
+
+
+
+void
+on_TEST_clicked                        (GtkWidget     *button2,
+                                        gpointer         user_data);
