73bf2ff0b508bd2a03ec9bad45cde28c2dc3100e
Omar <omartn7400@gmail.com>
2020-12-02 20:23:17 +0100

    final project

---
+++ Borj Lella/OUTBOX/src/callbacks.h
@@ -0,0 +1,105 @@
+#include <gtk/gtk.h>
+
+void
+on_button_ajouter_ouvrier_clicked      (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button_ajout_ouvrier_clicked        (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_cancelbutton_ajout_ouv_clicked      (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_okbutton_ajout_ouv_clicked          (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
+
+void
+on_radiobutton_ouv_male_clicked        (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_radiobutton_ouv_femelle_clicked     (GtkButton       *button,
+                                        gpointer         user_data);
+
+
+
+void
+on_button_affich_list_ouv_clicked      (GtkWidget       *objet,
+                                        gpointer         user_data);
+
+void
+on_treeview_affich_ouv_row_activated   (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_button_supp_ouvrier_clicked         (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button_modif_ouvrier_clicked        (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_cancelbutton_modif_ouvrier_clicked  (GtkWidget      *button,
+                                        gpointer         user_data);
+
+void
+on_okbutton_modif_ouvrier_clicked      (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button_rech_ouvrier_clicked         (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button_liste_ouv_point_clicked      (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button_rech_point_ouvrier_clicked   (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_treeview_affich_point_ouv_row_activated
+                                        (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_button_ajout_point_clicked          (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_button_liste_point_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_rech_point_clicked           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_treeview_liste_point_row_activated  (GtkTreeView     *treeview,
+                                        GtkTreePath     *path,
+                                        GtkTreeViewColumn *column,
+                                        gpointer         user_data);
+
+void
+on_button_modif_point_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_ajout_conge_clicked          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_button_affich_conge_clicked         (GtkButton       *button,
+                                        gpointer         user_data);
+
---
+++ me 3raftech chnowa nsamih/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ouvrier.h>
+
+void main(){
+	ouvrier 
+}
